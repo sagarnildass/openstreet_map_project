d717715a00581381babd6f1fe2fcde458beec580
Sagarnil Das <sagarnildass@gmail.com>
2017-06-16 18:07:55 +0530

    Update project_3_file_1.py

--- project_3_file_1.py
+++ project_3_file_1.py
@@ -13,7 +13,7 @@ Fill out the count_tags function. It should return a dictionary with the
 tag name as the key and number of times this tag can be encountered in 
 the map as value.
 
-Note that your code will be tested with a different data file than the 'example.osm'
+
 """
 import pprint
 import xml.etree.cElementTree as ET
@@ -49,12 +49,7 @@ Your task is to explore the data a bit more.
 Before you process the data and add it into MongoDB, you should
 check the "k" value for each "<tag>" and see if they can be valid keys in MongoDB,
 as well as see if there are any other potential problems.
-We have provided you with 3 regular expressions to check for certain patterns
-in the tags. As we saw in the quiz earlier, we would like to change the data model
-and expand the "addr:street" type of keys to a dictionary like this:
-{"address": {"street": "Some value"}}
-So, we have to see if we have such tags, and if we have any tags with problematic characters.
-Please complete the function 'key_type'.
+
 """
 import re
 lower = re.compile(r'^([a-z]|_)*$')
